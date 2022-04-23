4226f5a0f94dd2ab53e1cf4c462a7777c45ba444
s25535-pj <92204733+s25535-pj@users.noreply.github.com>
2022-04-23 17:03:04 +0200

    Add files via upload

---
+++ PracaDomowaNa_23_04_22/Wejsciowka_23_04_22/PrzedstawiaEmocje.java
@@ -0,0 +1,7 @@
+package Wejsciowka_23_04_22;
+
+public interface PrzedstawiaEmocje {
+
+    boolean czySieUsmiecha();
+    void przestanSieUsmiechac();
+}
---
+++ PracaDomowaNa_23_04_22/Wejsciowka_23_04_22/Rysowalny.java
@@ -0,0 +1,5 @@
+package Wejsciowka_23_04_22;
+
+public interface Rysowalny {
+    void draw();
+}
